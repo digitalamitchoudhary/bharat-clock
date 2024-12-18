5b9c076bbab5d20777a81e268a30be04d1df33aa
digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2024-12-19 00:15:14 +0530

    bharat Clock done

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,11 @@ function App() {
       <div className="app">
         <div className="watch">  <h1 className="my-3 text-center fw-bold lh-1">Bharat Clock</h1>
           <p className="fs-4 "> This is the clock that shows the Time & Date of Bharat </p>
-        </div>
-
-        <IndiaTime />
-        <div>
-          <IndiaDate />
-        </div>
-         <a className=" text-white text-decoration-none " href="https://zaap.bio/digitalamitchoudhary"> Made with <span >❤</span> by Digitalamitchoudhary
+        </div><IndiaTime />
+        <IndiaDate />
+        <a className=" text-white text-decoration-none " href="https://zaap.bio/digitalamitchoudhary"> Made with <span >❤</span> by Digitalamitchoudhary
         </a>
-        
+
       </div>
 
     </>
